83206a61492bbdbf5ae096b7f3a078eafa4e4f1f
Kyle Banashek <kylebanashek@yahoo.com>
2015-02-16 16:59:05 -0700

    Merge remote-tracking branch 'openshift/master'
    Conflicts:
    	package.json

--- server/config/express.js
+++ server/config/express.js
@@ -31,13 +31,13 @@ module.exports = function(app) {
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
-    app.use(require('connect-livereload')());
+
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
   }
 
   if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
+
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
