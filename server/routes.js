d9f75e602487937da778f9a4d295277d8a67acef
Kyle Banashek <kylebanashek@yahoo.com>
2015-02-16 17:47:47 -0700

    Merge remote-tracking branch 'openshift/master'
    Conflicts:
    	package.json

--- server/routes.js
+++ server/routes.js
@@ -22,6 +22,6 @@ module.exports = function(app) {
   app.route('/*')
     .get(function(req, res) {
      // res.sendfile(app.get('appPath') + '/index.html');
-      res.sendfile( express.static(__dirname + '/dist/public/index.html') );
+      res.sendfile( __dirname + '/dist/public/index.html' );
     });
 };
