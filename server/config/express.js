a8ef81dced8033044a86a1ad542f63c3cb22e009
Kyle Banashek <Kyle Banashek>
2016-06-13 16:09:46 -0600

    Built yourfund-fullstack from commit a2e598f on branch YF_1

--- server/config/express.js
+++ server/config/express.js
@@ -42,7 +42,8 @@ module.exports = function(app) {
 
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
+
+    app.set('appPath', config.root + '/client');
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
